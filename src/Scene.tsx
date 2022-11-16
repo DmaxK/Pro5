50b42e8613638681f3b51ced66156074b8fac262
DmaxK <75308901+DmaxK@users.noreply.github.com>
2022-11-16 22:51:49 +0100

    added drei and fiber, created a basic scene

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
+import './Scene.css';
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="FireBrick" />
+    </mesh>
+  );
+}
+
+function Plane() {
+  return (
+    <mesh position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <boxBufferGeometry attach="geometry" args={[20, 20]} />
+      <meshLambertMaterial attach="material" color="LightSlateGrey" />
+    </mesh>
+  );
+}
 
 function Scene() {
-  return <p>Scene</p>;
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Box />
+      <Plane />
+    </Canvas>
+  );
 }
 
 export default Scene;
