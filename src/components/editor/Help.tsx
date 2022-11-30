9e2a43871a0c8b732877ba654124599d19019b49
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2022-11-30 11:20:34 +0100

    ui touches

--- src/components/editor/Help.tsx
+++ src/components/editor/Help.tsx
@@ -10,17 +10,17 @@ const Help = () => {
                 ?
             </button>
             {expanded && <div className="helpContent">
-                <ul>
+                <ol>
                     <li>
-                    To look around, press ....... epic explanation of controls 
+                    To look around, click, hold and drag the mouse. 
                     </li>
                     <li>
-                    To move, press ..... another epic explanation 
+                    To move forward/backward, scroll up/down.
                     </li>
                     <li>
-                    To Place an image, do this and that
+                    To Place an image, click the "Place" button on the top left. Then click, hold and drag anywhere in the 3D scene.  
                     </li>
-                </ul>
+                </ol>
             </div>}
         </div>
     );
