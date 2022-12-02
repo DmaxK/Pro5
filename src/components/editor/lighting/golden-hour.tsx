3f94c48f7ce0346977ee3ed43e1d39354ea94d2b
Pilegrim <pilegrim60@gmail.com>
2022-12-02 11:16:25 +0100

    added clouds and sky

--- src/components/editor/lighting/golden-hour.tsx
+++ src/components/editor/lighting/golden-hour.tsx
@@ -1,11 +1,20 @@
-import { Environment } from '@react-three/drei'
+import { Environment, Sky, Cloud } from '@react-three/drei'
 
 const Goldenhour = () => {
   return (
     <>
-        {/*<hemisphereLight color={"#badbf5"} intensity={0.0}/>*/}
         <directionalLight color={"#f5ad6e"} castShadow={true} position={[5, 4, 6]} intensity={0.4}/>
-        <Environment files="../../../../public/images/sunset6.hdr" background />
+        <Environment files="../../../../public/images/sunset6.hdr"/>
+        < Sky inclination={0.4} turbidity={0.3} azimuth={-180} rayleigh={1.5} sunPosition={[5, 0.3, 6]}/>
+        <Cloud position={[40, 50, 20]} speed={0.1} opacity={1} segments={8}  width={30}/>
+        <Cloud position={[-35, 35, 30]} speed={0.1} opacity={1} segments={8}  width={40}/>
+        <Cloud position={[30, 40, -35]} speed={0.1} opacity={1} segments={8}  width={20}/>
+        <Cloud position={[-27, 40, -20]} speed={0.1} opacity={1} segments={8}  width={25}/>
+        <Cloud position={[-2, 60, -3]} speed={0.1} opacity={1} segments={8}  width={25}/>
+        <Cloud position={[3, 50, 70]} speed={0.1} opacity={1} segments={8}  width={30}/>
+        <Cloud position={[-80, 35, 2]} speed={0.1} opacity={1} segments={8}  width={40}/>
+        <Cloud position={[75, 40, -4]} speed={0.1} opacity={1} segments={8}  width={20}/>
+        <Cloud position={[-3, 40, -85]} speed={0.1} opacity={1} segments={8}  width={25}/>
         </>
   )
 }
--- src/components/editor/lighting/noon.tsx
+++ src/components/editor/lighting/noon.tsx
@@ -1,12 +1,21 @@
-import { Environment } from '@react-three/drei'
+import { Environment, Sky, Cloud } from '@react-three/drei'
 
 
 const Noon = () => {
     return(
         <>
-        <hemisphereLight color={"#badbf5"} intensity={0.4}/>
-        <directionalLight color={"#fffbd4"} castShadow={true} position={[2, 10, 2]} intensity = {1}/>
-        <Environment files="../../../../public/images/cloudy_sky.hdr" background/>
+        <directionalLight color={"#fffbd4"} castShadow={true} position={[2, 10, 2]} intensity = {0.7}/>
+        <Environment files="../../../../public/images/cloudy_sky.hdr"/>
+        < Sky distance={30000} inclination={1} turbidity={0.3} azimuth={-90} rayleigh={0.1} mieCoefficient={0.1} mieDirectionalG={0.3} sunPosition={[2, 10, 2]}/>
+        <Cloud position={[40, 50, 20]} speed={0.1} opacity={1} segments={8}  width={30}/>
+        <Cloud position={[-35, 35, 30]} speed={0.1} opacity={1} segments={8}  width={40}/>
+        <Cloud position={[30, 40, -35]} speed={0.1} opacity={1} segments={8}  width={20}/>
+        <Cloud position={[-27, 40, -20]} speed={0.1} opacity={1} segments={8}  width={25}/>
+        <Cloud position={[-2, 60, -3]} speed={0.1} opacity={1} segments={8}  width={25}/>
+        <Cloud position={[3, 50, 70]} speed={0.1} opacity={1} segments={8}  width={30}/>
+        <Cloud position={[-80, 35, 2]} speed={0.1} opacity={1} segments={8}  width={40}/>
+        <Cloud position={[75, 40, -4]} speed={0.1} opacity={1} segments={8}  width={20}/>
+        <Cloud position={[-3, 40, -85]} speed={0.1} opacity={1} segments={8}  width={25}/>
         </>
     )
 }
