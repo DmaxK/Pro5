b017102bd2e993f7b157516be82a3a5f82654e09
Pilegrim <pilegrim60@gmail.com>
2022-12-07 18:44:52 +0100

    scene 1 imported and minor tweaks

--- src/components/editor/lighting/golden-hour.tsx
+++ src/components/editor/lighting/golden-hour.tsx
@@ -3,8 +3,8 @@ import { Environment, Sky, Cloud } from '@react-three/drei'
 const Goldenhour = () => {
   return (
     <>
-        <directionalLight color={"#f5ad6e"} castShadow={true} position={[5, 4, 6]} intensity={0.4}/>
-        <Environment files="../../../../public/images/sunset6.hdr"/>
+        <directionalLight color={"#f5ad6e"} castShadow={true} position={[5, 4, 6]} intensity={0.2}/>
+        <Environment files="/images/sunset6.hdr"/>
         < Sky inclination={0.4} turbidity={0.3} azimuth={-180} rayleigh={1.5} sunPosition={[5, 0.3, 6]}/>
         <Cloud position={[40, 50, 20]} speed={0.1} opacity={1} segments={8}  width={30}/>
         <Cloud position={[-35, 35, 30]} speed={0.1} opacity={1} segments={8}  width={40}/>
