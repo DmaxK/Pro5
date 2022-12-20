65173ab3da9543daab8ceff891e8dede5b92e515
Pilegrim <pilegrim60@gmail.com>
2022-12-20 15:11:07 +0100

    look pretty much done

--- src/components/editor/POI.tsx
+++ src/components/editor/POI.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber';
-import { MeshPhongMaterial, Vector3 } from 'three';
+import { Color, DoubleSide, MeshPhongMaterial, Vector3 } from 'three';
 import React, { Dispatch, useEffect, useRef, useState } from 'react';
 
 const POI: React.FC<{position: Vector3, setCameraPosition: Dispatch<React.SetStateAction<Vector3>>}> = ({position, setCameraPosition}) => {
@@ -9,14 +9,15 @@ const POI: React.FC<{position: Vector3, setCameraPosition: Dispatch<React.SetSta
 	useEffect(() => {
   		document.body.style.cursor = hovered ? 'pointer' : 'auto'
 		if(mat.current){
-			mat.current.opacity = hovered ? 0.5 : 0.3;
-			//mat.current.color = hovered ? "#F88DDF" : "#F88DD5";
+			console.log("bruh")
+			mat.current.opacity = hovered ? 0.8 : 0.5;
+			mat.current.color = hovered ? new Color(0xF88DDF)  : new Color(0xF88DD5);
 		}
 	}, [hovered])
     return(
-    <mesh onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} position={position} scale={[0.5,0.5,0.5]} onClick={() => setCameraPosition(position)}>
+    <mesh onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} position={position} scale={[0.5,0.5,0.5]} onClick={() => setCameraPosition(new Vector3(position.x, position.y + 2, position.z))}>
         <sphereBufferGeometry attach={"geometry"} args={[1, 32, 32, 0, 2*Math.PI, 0, Math.PI/2]}/>
-        <meshPhongMaterial color={"#F88DD5"} opacity={0.3} emissive={"#F88DD5"} emissiveIntensity={1} transparent />
+        <meshPhongMaterial ref={mat} color={"#F88DD5"} opacity={0.5} emissive={"#F88DD5"} emissiveIntensity={1} transparent/>
     </mesh>
     );
 }
