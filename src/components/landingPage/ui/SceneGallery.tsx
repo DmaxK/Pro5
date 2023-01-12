fb1d7258f47e5f9fd304d847d5d270cf989c1edb
DmaxK <75308901+DmaxK@users.noreply.github.com>
2023-01-12 19:20:11 +0100

    route testing

--- src/components/landingPage/ui/SceneGallery.tsx
+++ src/components/landingPage/ui/SceneGallery.tsx
@@ -5,8 +5,8 @@ import GalleryItem from './GalleryItem';
 function SceneGallery() {
   return (
     <div className="gallery">
-      <GalleryItem URL={'/images/summonersrift.jpg'} gifURL={'/images/summonersrift.gif'} alt="Summoners Rift" />
-      <GalleryItem URL={'/images/rocketleague.jpg'} gifURL={'/images/summonersrift.gif'} alt="Rocket League" />
+      <GalleryItem URL={'./images/summonersrift.jpg'} gifURL={'/images/summonersrift.gif'} alt="Summoners Rift" />
+      <GalleryItem URL={'/Pro5/images/rocketleague.jpg'} gifURL={'/images/summonersrift.gif'} alt="Rocket League" />
       <GalleryItem URL={'/images/kingsrow.jpg'} gifURL={'/images/summonersrift.gif'} alt="Kings Row" />
     </div>
   );
