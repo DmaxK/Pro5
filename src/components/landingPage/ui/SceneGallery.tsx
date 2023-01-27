54f6542317d32c920af8f5e4784f86066bbda4bb
DmaxK <75308901+DmaxK@users.noreply.github.com>
2023-01-27 13:06:42 +0100

    added thumbnail and gif for Scene2

--- src/components/landingPage/ui/SceneGallery.tsx
+++ src/components/landingPage/ui/SceneGallery.tsx
@@ -6,7 +6,7 @@ function SceneGallery() {
   return (
     <div className="gallery">
       <GalleryItem id={1} URL={'./images/scene_1_thumbnail.png'} gifURL={'./gifs/scene_1_image_gallery.gif'} alt="Scene_1_Thumbnail" />
-      <GalleryItem id={2} URL={'./images/scene_2_thumbnail.png'} gifURL={'./gifs/summonersrift.gif'} alt="Scene_2_Thumbnail" />
+      <GalleryItem id={2} URL={'./images/scene_2_thumbnail.png'} gifURL={'./gifs/scene_2_image_gallery.gif'} alt="Scene_2_Thumbnail" />
       {/* <GalleryItem URL={'./images/kingsrow.jpg'} gifURL={'./images/summonersrift.gif'} alt="Kings Row" /> */}
     </div>
   );
