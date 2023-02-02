dad07caa79f2e6ed799cf8526e7885db7ff55576
DmaxK <75308901+DmaxK@users.noreply.github.com>
2023-02-02 20:03:38 +0100

    compressed thumbnails and gifs

--- src/components/landingPage/ui/SceneGallery.tsx
+++ src/components/landingPage/ui/SceneGallery.tsx
@@ -5,8 +5,8 @@ import GalleryItem from './GalleryItem';
 function SceneGallery() {
   return (
     <div className="gallery">
-      <GalleryItem id={1} URL={'./images/scene_1_thumbnail.png'} gifURL={'./gifs/scene_1_image_gallery.gif'} alt="Scene_1_Thumbnail" />
-      <GalleryItem id={2} URL={'./images/scene_2_thumbnail.png'} gifURL={'./gifs/scene_2_image_gallery.gif'} alt="Scene_2_Thumbnail" />
+      <GalleryItem id={1} URL={'./images/scene_1_thumbnail-min.png'} gifURL={'./gifs/scene_1_image_gallery.gif'} alt="Scene_1_Thumbnail" />
+      <GalleryItem id={2} URL={'./images/scene_2_thumbnail-min.png'} gifURL={'./gifs/scene_2_image_gallery.gif'} alt="Scene_2_Thumbnail" />
       {/* <GalleryItem URL={'./images/kingsrow.jpg'} gifURL={'./images/summonersrift.gif'} alt="Kings Row" /> */}
     </div>
   );
